2de528340e90376f032d9f32f21de9f62898bfae
hnarita <hnarita@seioukai.jp>
2025-09-21 20:59:28 +0900

    Add JSDoc documentation to portal.js functions
    - Added comprehensive JSDoc headers for addApproverFromApprovalGroup function
    - Added JSDoc documentation for showErrorMessage function
    - Improved code readability with proper parameter and return type documentation

--- public/assets/js/portal.js
+++ public/assets/js/portal.js
@@ -1,7 +1,15 @@
 
 
 
-
+/**
+ * 承認グループマスタから承認者リストを取得し、指定の承認ステップに設定する
+ *
+ * @param {Object} form - SateraitoWFのフォームオブジェクト
+ * @param {string} groupId - 承認グループID
+ * @param {number} stepNo - 承認ステップ番号
+ * @param {string} [defaultApprover] - デフォルト承認者（グループに登録がない場合の代替）
+ * @returns {void}
+ */
 function addApproverFromApprovalGroup(form, groupId, stepNo, defaultApprover) {
 
   // 承認グループマスタから承認者リストを取得し指定の承認ステップに設定する
@@ -19,3 +27,22 @@ function addApproverFromApprovalGroup(form, groupId, stepNo, defaultApprover) {
   });
 
 }
+
+/**
+ * エラーメッセージダイアログを表示し、指定したフィールドにフォーカスを設定する
+ *
+ * @param {Object} form - jQueryフォームオブジェクト
+ * @param {string} title - ダイアログのタイトル
+ * @param {string} message - 表示するエラーメッセージ
+ * @param {string} nameAttribute - フォーカスを設定するフィールドのname属性値
+ * @returns {void}
+ */
+function showErrorMessage(form, title, message, nameAttribute){
+
+    SateraitoWF.alert(`${message}`, `${title}`, function(is_ok){
+        if(is_ok){
+            $(form).find(`[name=${nameAttribute}]`).focus();
+        }
+    });
+
+};
